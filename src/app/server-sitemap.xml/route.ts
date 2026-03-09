ebe17c1a8fdf35d21d5458c6ec657833e5201648
JackyYang <jackyyang@uanalyze.com.tw>
2026-03-09 11:41:55 +0800

    chage server sitemap to all languages

--- src/app/server-sitemap.xml/route.ts
+++ src/app/server-sitemap.xml/route.ts
@@ -4,21 +4,28 @@ import { PostDoc } from "@/schema/type/post";
 import { getServerSideSitemap } from "next-sitemap";
 import { client } from "@/sanity/lib/client";
 import { NextRequest } from "next/server";
+import { languages } from "@/i18n/config";
 export async function GET(req: NextRequest) {
   const origin = req.nextUrl.origin;
   const posts = await client.fetch<PostDoc[]>(
     `*[_type == "post"] | order(_createdAt desc) {
       "slug": slug.current,
       _createdAt
-    }`
+    }`,
   );
 
-  const fields = posts.map((p) => ({
-    loc: `${origin}/post/${p.slug}`,
-    lastmod: new Date(p._createdAt ?? Date.now()).toISOString(),
-    changefreq: "weekly" as const,
-    priority: 0.7,
-  }));
+  const fields = posts.flatMap((p) =>
+    languages.map((lng) => ({
+      loc: `${origin}/${lng}/post/${p.slug}`,
+      lastmod: new Date(p._createdAt ?? Date.now()).toISOString(),
+      alternateRefs: languages.map((l) => ({
+        href: `${origin}/${l}/post/${p.slug}`,
+        hreflang: l,
+      })),
+    })),
+  );
+
+  console.log(fields);
 
   return getServerSideSitemap(fields);
 }
